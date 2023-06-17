a832ce5f424caded6f8d0c4fdca303877fa11e88
aydotjs <ayobamiseyiowoeye@gmail.com>
2023-06-17 09:19:16 +0100

    final commit on guess my number game

--- 04-Guess-My-Number/starter/script.js
+++ 04-Guess-My-Number/starter/script.js
@@ -1,45 +1,52 @@
 'use strict';
-const bodyElement = document.querySelector('body');
-const guessedNumber = document.querySelector('.guess');
-const message = document.querySelector('.message');
-const checkBtn = document.querySelector('.check');
-const newGameBtn = document.querySelector('.again');
-const highScore = document.querySelector('.highscore');
-const scoreEl = document.querySelector('.score');
-const randomNumberEl = document.querySelector('.number');
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
-randomNumberEl.textContent = randomNumber;
+const bodyElement = document.querySelector('body'),
+  guessedNumber = document.querySelector('.guess'),
+  message = document.querySelector('.message'),
+  checkBtn = document.querySelector('.check'),
+  newGameBtn = document.querySelector('.again'),
+  highScoreEl = document.querySelector('.highscore'),
+  scoreEl = document.querySelector('.score'),
+  randomNumberEl = document.querySelector('.number');
+  
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
+const displayMessage = function (msg) {
+  message.textContent = msg;
+};
 // IMPLEMENTING GAME LOGIC
 checkBtn.addEventListener('click', function () {
   const guessedNumberValue = Number(guessedNumber.value);
   if (!guessedNumberValue) {
-    message.textContent = '🛑 No Number';
-  } 
-  
-  else if (guessedNumberValue < randomNumber) {
+    displayMessage('No Number');
+  } else if (guessedNumberValue !== randomNumber) {
     if (score > 0) {
-      message.textContent = '😒 Too Low';
+      message.textContent =
+        guessedNumberValue > randomNumber ? '⛈️Too High' : '😒Too Low';
       score--;
       scoreEl.textContent = score;
     } else {
-      message.textContent = 'You lost the game';
+      displayMessage('You lost the game');
     }
-  }
-  
-  else if (guessedNumberValue > randomNumber) {
-    if (score > 0) {
-      message.textContent = '⛈️ Too High';
-      score--;
-      scoreEl.textContent = score;
-    } else {
-      message.textContent = 'You lost the game';
-    }
-  }
-
-  else if (guessedNumberValue === randomNumber) {
-    message.textContent = '🙌 You are correct';
+  } else if (guessedNumberValue === randomNumber) {
+    displayMessage('🙌 You are correct');
     bodyElement.style.backgroundColor = '#60af42';
     randomNumberEl.style.width = '30rem';
+    randomNumberEl.textContent = randomNumber;
+    if (score > highScore) {
+      highScore = score;
+      highScoreEl.textContent = highScore;
+    }
   }
 });
+// RESETTING THE GAME
+newGameBtn.addEventListener('click', function () {
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  score = 20;
+  bodyElement.style.backgroundColor = '#222';
+  scoreEl.textContent = score;
+  guessedNumber.value = '';
+  randomNumberEl.textContent = '?';
+});
+
